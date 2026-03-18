428c5ea336618a916a532ccb5ad43e8813697399
Andrea Bujold <Andrea@Andreas-MacBook-Pro-2.local>
2026-03-18 16:42:29 -0400

    terminal output fixed

--- intro.py
+++ intro.py
@@ -9,7 +9,7 @@ the program will then call the appropriate function to answer the question.
 import subprocess
 
 def intro():
-    print("Welcome to the Ontario Election and Inflation Analysis Program! \n ___________________________________________________________")
+    print("Welcome to the Ontario Election and Inflation Analysis Program!\n______________________________________________________________________________________")
     print("This program will analyze the relationship between Ontario's inflation rates and the vote percentages of all the major political parties in the 2004, 2006, 2008, 2011, 2015, 2019, 2021 and 2025 elections. \n")
     print("Your choices for analysis are: \n 1. How does pre-election inflation in Ontario correlate with the change in the number of votes that a specific party receives? \n 2. How does the density of a specific age demographic in Ontario correlate with the voter turnout (ie, the total number of votes cast)?")
     choice = input("Please enter the number corresponding to the question you want answered (1 or 2): ")
