7b710adbaa2fb4b3dac2066227ae0ee89c2e8f92
SkyRED119 <amir.kayumov7@gmail.com>
2026-03-18 16:43:54 -0400

    Made the execution in intro.py more consistent among platforms

--- intro.py
+++ intro.py
@@ -7,6 +7,7 @@ the program will then call the appropriate function to answer the question.
 """
 
 import subprocess
+import sys
 
 def intro():
     print("Welcome to the Ontario Election and Inflation Analysis Program!\n______________________________________________________________________________________")
@@ -14,9 +15,9 @@ def intro():
     print("Your choices for analysis are: \n 1. How does pre-election inflation in Ontario correlate with the change in the number of votes that a specific party receives? \n 2. How does the density of a specific age demographic in Ontario correlate with the voter turnout (ie, the total number of votes cast)?")
     choice = input("Please enter the number corresponding to the question you want answered (1 or 2): ")
     if choice == "1":
-        subprocess.run(["python3", "analyzer_q1.py", "processed_inflation.csv", "processed_vote_percentages.csv"], check=True)
+        subprocess.run([sys.executable, "analyzer_q1.py", "processed_inflation.csv", "processed_vote_percentages.csv"], check=True)
     elif choice == "2":
-        subprocess.run(["python3", "analyzer_q2.py", "processed_age_turnout.csv", "processed_vote_percentages.csv"], check=True)  
+        subprocess.run([sys.executable, "analyzer_q2.py", "processed_age_turnout.csv", "processed_vote_percentages.csv"], check=True)  
     else:
         print("Invalid choice. Please enter 1 or 2.")
 
